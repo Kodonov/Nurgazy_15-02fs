42a0e1869d477338cdd9f7d1053df5d31e81c76c
Kodonov <93920158+Kodonov@users.noreply.github.com>
2022-06-14 06:05:58 -0500

    nurik

--- lesson1/main.js
+++ lesson1/main.js
@@ -114,53 +114,89 @@
 
 // **************************Первое задание*******************************
 
-const limit = 7 
-let counter = 0
+// const limit = 7 
+// let counter = 0
 
-while(counter <= limit) {
-    let result = ''
-    for (let i = 0; i < counter; i++) {
-        result += '#'
-    }
-    console.log(result)
-    counter++
-}
+// while(counter <= limit) {
+//     let result = ''
+//     for (let i = 0; i < counter; i++) {
+//         result += '#'
+//     }
+//     console.log(result)
+//     counter++
+// }
 
-// **************************Второе задание*******************************
-
-for (counter = 1; counter <= 100; counter ++) {
-    if (counter % 3 == 0)
-           console.log('FIZ');
-    else if ((counter % 3 != 0) && (counter % 5 == 0))
-           console.log('BUZ');
-        else if ((counter % 3 == 0) && (counter % 5 == 0))
-           console.log('FIZBUZ');
-    else
-       console.log(counter);
-}
-
-
-
-
-
-
-
-
-
-
-
-
+// // **************************Второе задание*******************************
 
 // for (counter = 1; counter <= 100; counter ++) {
-    
-//     if ((counter % 3 == 0) && (counter % 5 == 0))
-//            console.log('FizzBuzz');
-//            else
 //     if (counter % 3 == 0)
-//            console.log('Fizz');
-//            else
-//     if ((counter % 3 != 0) && (counter % 5 == 0))
-//            console.log('Buzz');
-//                 else
+//            console.log('FIZ');
+//     else if ((counter % 3 != 0) && (counter % 5 == 0))
+//            console.log('BUZ');
+//         else if ((counter % 3 == 0) && (counter % 5 == 0))
+//            console.log('FIZBUZ');
+//     else
 //        console.log(counter);
+
+// function getFullName(user, age) {
+//     if(age < 18){
+//         alert("Close")
+//         }else{
+//             alert(user.first_name + " " + user.last_name + " " + "open")
+//     }
 // }
+
+// const user = {
+//     first_name: "John",
+//     last_name: "Barbaro"
+// }
+
+// const age = prompt("Скаолько вам лет?")
+
+// getFullName(user, age)
+
+// const anonym = function (users) {
+//     console.log(user, users)
+// }
+// anonym()
+
+// let button;
+
+// button.onlick = function () {
+
+// }
+
+
+//***********************************1 ЗАДАНИЕ 3 УРОКА */
+// function numbers(number1, number2){
+//     if(number1 > number2){
+//         console.log("Первое больше")
+//     }else if(number2 > number1){
+//         console.log("Второе больше")
+//     }else {
+//         console.log("Они равны")
+//     }
+// }
+
+// const number = {
+//     number1: prompt("число?"),
+//     number2: prompt("число?")
+// }
+// numbers(number.number1, number.number2)
+
+/////////////////////////////////////2 ЗАДАНИЕ 3 УРОКА
+
+const findLenghtAndPosition = (str, char) =>{
+    const strLength = str.length;
+    const position =  str.indexOf(char) != -1 ? str.indexOf(char) : 0;
+    return {
+        length: strLength,
+        position: position
+    }
+}
+
+console.log('Find: ', findLenghtAndPosition('Temirlan sensey', 's')); //{ length: 15, position: 9 }
+
+
+
+
